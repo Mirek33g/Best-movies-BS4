a856ba1d461421c5e1e97a92ade0e50dea3d51cc
Mirek33g <133931969+Mirek33g@users.noreply.github.com>
2023-11-02 07:07:31 +0000

    Beautiful Soup and scraping from website

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import requests
 import bs4
 
+# gets API resonse from the website
 response = requests.get('https://www.empireonline.com/movies/features/best-movies-2/')
 response.raise_for_status()
 
+# creates Beautiful Soup object ale searches for items on website
 soup = bs4.BeautifulSoup(response.text, 'html.parser')
 movies = soup.find_all(class_= 'listicleItem_listicle-item__title__hW_Kn')
+# loops through all the movies and prints them out
 i = 1
 for movie in movies:
     all_movies = movie.text.split(') ')
